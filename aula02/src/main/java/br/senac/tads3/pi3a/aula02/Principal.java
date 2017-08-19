01f1415fa4e6b4f4dbe4c2639f2221d0e6391ac3
Diogoogoid <dgo.contato@hotmail.com>
2017-08-18 21:40:24 -0300

    git commit -m "Versão inicial"

---
+++ aula02/src/main/java/br/senac/tads3/pi3a/aula02/Principal.java
@@ -0,0 +1,14 @@
+package br.senac.tads3.pi3a.aula02;
+
+/**
+ *
+ * @author diogo.sfelix
+ */
+public class Principal {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Word Marven");
+        
+        
+    }
+}
